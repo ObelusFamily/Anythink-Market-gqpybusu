f84ce19664a93b9bd8c394949ffcd10514416051
Jose Pablo Feng <jpfeng-dev@proton.me>
2023-01-29 04:35:06 +0000

    Reverted and used promise to call action

--- frontend/src/components/Item/CommentInput.js
+++ frontend/src/components/Item/CommentInput.js
@@ -1,24 +1,74 @@
 import React, { useState } from "react";
 import agent from "../../agent";
+import { connect } from "react-redux";
 import { ADD_COMMENT } from "../../constants/actionTypes";
-import { useDispatch } from 'react-redux'
 
-export default function CommentInput({
-  currentUser, 
-  slug,
-}) {
+
+const mapDispatchToProps = (dispatch) => ({
+  onSubmit: (payload) => dispatch({ type: ADD_COMMENT, payload }),
+});
+
+// class CommentInput extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       body: "",
+//     };
+
+//     this.setBody = (ev) => {
+//       this.setState({ body: ev.target.value });
+//     };
+
+//     this.createComment = async (ev) => {
+//       ev.preventDefault();
+//       agent.Comments.create(this.props.slug, {
+//         body: this.state.body,
+//       }).then((payload) => {
+//         this.props.onSubmit(payload);
+//       });
+//       this.setState({ body: "" });
+//     };
+//   }
+
+//   render() {
+//     return (
+//       <form className="card comment-form m-2" onSubmit={this.createComment}>
+//         <div className="card-block">
+//           <textarea
+//             className="form-control"
+//             placeholder="Write a comment..."
+//             value={this.state.body}
+//             onChange={this.setBody}
+//             rows="3"
+//           ></textarea>
+//         </div>
+//         <div className="card-footer">
+//           <img
+//             src={this.props.currentUser.image}
+//             className="user-pic mr-2"
+//             alt={this.props.currentUser.username}
+//           />
+//           <button className="btn btn-sm btn-primary" type="submit">
+//             Post Comment
+//           </button>
+//         </div>
+//       </form>
+//     );
+//   }
+// }
+
+function CommentInput({currentUser, slug, onSubmit}) {
   const [body, setBody] = useState("")
-
-  const dispatch = useDispatch()
 
   async function createComment(e) {
     e.preventDefault()
     
-    const payload = await agent.Comments.create(slug, {
+    agent.Comments.create(slug, {
       body
+    }).then((payload) => {
+      onSubmit(payload)
     });
 
-    dispatch({ type: ADD_COMMENT, payload })
     setBody("")
   }
 
@@ -46,3 +96,5 @@ export default function CommentInput({
       </form>
   )
 }
+
+export default connect(() => ({}), mapDispatchToProps)(CommentInput);
